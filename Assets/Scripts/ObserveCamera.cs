370346b4f966a568b81754f45ab40a10e6228afa
Park SangHyeok <tkdgurehf00@gmail.com>
2025-03-05 10:35:09 +0900

    Add ObserveCamera.cs

---
+++ Assets/Scripts/ObserveCamera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+public class ObserveCamera : MonoBehaviour
+{
+    public GameObject toObserve;
+
+    private float fov;
+    private float startDistance;
+
+    private void Awake()
+    {
+        fov = Camera.main.fieldOfView;
+        startDistance = (toObserve.transform.position - transform.position).magnitude;
+    }
+
+    void Update()
+    {
+        float distance = (toObserve.transform.position - transform.position).magnitude;
+
+        var newFov = fov * startDistance / distance;
+        Camera.main.fieldOfView = newFov;
+
+        var lookDir = toObserve.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(lookDir);
+    }
+}
+}
