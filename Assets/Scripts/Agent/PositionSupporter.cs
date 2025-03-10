b03b236a18e97d89ea22dd6df9334e771689f51d
Park SangHyeok <tkdgurehf00@gmail.com>
2025-03-10 09:16:11 +0900

    Fix PositionSupporter to save Target

--- Assets/Scripts/Agent/PositionSupporter.cs
+++ Assets/Scripts/Agent/PositionSupporter.cs
@@ -4,15 +4,17 @@ namespace Agent
 {
 public class PositionSupporter : MonoBehaviour
 {
-    public void UpdateOrientation(Transform rootBP, Vector3 dirVector)
+    public GameObject targetObject;
+
+    public void UpdateOrientation(Transform rootBP)
     {
-        dirVector.y = 0; //flatten dir on the y. this will only work on level, uneven surfaces
+        var dirVector = targetObject.transform.position - transform.position;
+        dirVector.y = 0;
         var lookRot =
             dirVector == Vector3.zero
                 ? Quaternion.identity
-                : Quaternion.LookRotation(dirVector); //get our look rot to the target
+                : Quaternion.LookRotation(dirVector);
 
-        //UPDATE ORIENTATION CUBE POS & ROT
         transform.SetPositionAndRotation(rootBP.position, lookRot);
     }
 }
