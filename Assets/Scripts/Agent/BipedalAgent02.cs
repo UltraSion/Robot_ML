76807ee07dd48c4b602207052779bc50fbd6f1b6
Park SangHyeok <tkdgurehf00@gmail.com>
2025-03-10 09:14:12 +0900

    Add BipedalAgentSetting

--- Assets/Scripts/Agent/BipedalAgent02.cs
+++ Assets/Scripts/Agent/BipedalAgent02.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.Generic;
 using Controller;
-using NUnit.Framework;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace Agent
@@ -12,9 +10,8 @@ namespace Agent
 public class BipedalAgent02 : Unity.MLAgents.Agent
 {
     private BipedalController02 bipedalController;
-    public ArticulationBody body;
 
-    public Vector3 startPos;
+    private Vector3 startPos;
 
     private List<float> backUpPos = new List<float>();
     private List<float> backUpVel = new List<float>();
@@ -22,17 +19,26 @@ public class BipedalAgent02 : Unity.MLAgents.Agent
 
     private List<ArticulationDrive> startState = new();
 
-    public List<WalkChecker> Checkers;
+    public GameObject targetObject;
+
+    public BipedalAgentSetting setting;
+
+    public float forceTimer = 0;
+
+    [SerializeField] [Range(0, 1)] private float averageSpeedReward = 0;
+    public float Avg_speedReward => averageSpeedReward;
+    public float CurStep { private set; get; } = 0;
 
     protected override void Awake()
     {
         base.Awake();
         bipedalController = GetComponent<BipedalController02>();
-
     }
 
     private void Start()
     {
+        setting = BipedalAgentSetting.instance;
+
         startPos = transform.position;
         bipedalController.pelvis.GetJointPositions(backUpPos);
         bipedalController.pelvis.GetJointVelocities(backUpVel);
@@ -45,8 +51,7 @@ public class BipedalAgent02 : Unity.MLAgents.Agent
 
     private void ResetBody()
     {
-        // bipedalController.pelvis.TeleportRoot(startPos, Quaternion.Euler(0, Random.Range(0.0f, 360.0f), 0));
-        bipedalController.pelvis.TeleportRoot(startPos, Quaternion.identity);
+        bipedalController.pelvis.TeleportRoot(startPos, Quaternion.Euler(0, Random.Range(0.0f, 360.0f), 0));
 
         bipedalController.pelvis.linearVelocity = Vector3.zero;
         bipedalController.pelvis.angularVelocity = Vector3.zero;
@@ -55,8 +60,6 @@ public class BipedalAgent02 : Unity.MLAgents.Agent
             controller.articulationBody.linearVelocity = Vector3.zero;
             controller.articulationBody.angularVelocity = Vector3.zero;
         });
-        body.linearVelocity = Vector3.zero;
-        body.angularVelocity = Vector3.zero;
 
         bipedalController.pelvis.SetJointPositions(backUpPos);
         bipedalController.pelvis.SetJointVelocities(backUpVel);
@@ -67,27 +70,34 @@ public class BipedalAgent02 : Unity.MLAgents.Agent
         }
     }
 
-    void Update()
+    private void RandTarget()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-            ResetBody();
+        bipedalController.TargetVelocity = Random.Range(setting.minSpeed, setting.maxSpeed);
+        RandTargetObject();
     }
 
-    public void RandTarget()
+    private void RandTargetObject()
     {
-        float moveDir = Random.Range(0.0f, 360.0f);
-        bipedalController.MoveDir = new Vector3(Mathf.Cos(moveDir), 0f, Mathf.Sin(moveDir));
+        float targetDir = Random.Range(0.0f, 360.0f);
+        float targetDistance = Random.Range(3f, 40f);
 
-        bipedalController.TargetVelocity = Random.Range(0.0001f, 10f);
-        // bipedalController.SetHeight(Random.Range(2f, 4.5f));
-        // bipedalController.SetSpeed(4f);
-        bipedalController.TargetHeight = 3f;
+        Vector3 targetPos = new Vector3(Mathf.Cos(targetDir) * targetDistance, 0.5f,
+            Mathf.Sin(targetDir) * targetDistance);
+        targetObject.transform.localPosition = targetPos;
     }
 
     public override void OnEpisodeBegin()
     {
+        OptimizeMaxSpeed();
+
+
         ResetBody();
+        bipedalController.TargetVelocity = Random.Range(setting.minSpeed, setting.maxSpeed);
+
         RandTarget();
+
+        averageSpeedReward = 0f;
+        CurStep = 0f;
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -113,36 +123,132 @@ public class BipedalAgent02 : Unity.MLAgents.Agent
         bipedalController.SetDrive(forceRatios, targets);
     }
 
-    private bool IsJumping()
+    private bool IsTargetTouched()
     {
-        bool isJumping = true;
-
-        foreach (var checker in Checkers)
-        {
-            if (checker.IsGround)
-                isJumping = false;
-        }
-
-        return isJumping;
+        var positionDelta = (transform.position - targetObject.transform.position);
+        positionDelta.y = 0f;
+        float distance = positionDelta.magnitude;
+        return distance < 0.5f;
     }
 
     private void FixedUpdate()
     {
-        if (IsJumping())
+        // if (IsJumping())
+        // {
+        //     Checkers.ForEach(checker => checker.Reset());
+        //     EndEpisode();
+        //     return;
+        // }
+
+        // float pelvisY = bipedalController.pelvis.transform.position.y;
+        if (IsTargetTouched())
         {
-            Checkers.ForEach(checker => checker.Reset());
-            EndEpisode();
-            return;
+            RandTargetObject();
+            AddReward(1f);
         }
 
-        float pelvisY = bipedalController.pelvis.transform.position.y;
+        if (forceTimer > setting.forceInterval)
+        {
+            var n1 = Random.Range(0f, 360f) * Mathf.Deg2Rad;
+            var n2 = Random.Range(0f, 360f) * Mathf.Deg2Rad;
 
-        float speedReward = bipedalController.VelocityAccuracy;
+            var x = Mathf.Sin(n1) * Mathf.Cos(n2);
+            var y = Mathf.Sin(n1) * Mathf.Sin(n2);
+            var z = Mathf.Cos(n1);
+            var force = setting.forcePower * Random.Range(0.5f, 1f) * new Vector3(x, y, z);
+            GetRandomParts().AddForce(force);
+            forceTimer = 0;
+        }
+        else
+        {
+            forceTimer += Time.fixedDeltaTime;
+        }
 
-        float lookReward = bipedalController.FootLookDot;
+        float speedReward = bipedalController.VelocityAccuracy2;
+        float pelvisUprightReward = (bipedalController.PelvisUprightDot + 1f) * 0.5f;
+        float pelvisLookReward = Mathf.Abs(bipedalController.LookDot);
+        float footReward = bipedalController.FootLookDot;
 
-        AddReward(1f);
-        AddReward(speedReward);
+        float efficiency = bipedalController.Efficiency;
+        float difficulty = speedReward * bipedalController.TargetVelocity / setting.maxSpeed;
+        float efficiencyReward = difficulty + efficiency * (1 - difficulty);
+
+        float reward = speedReward * efficiencyReward * pelvisLookReward * footReward;
+
+        CalculateAverageSpeedReward(speedReward);
+        AddReward(reward);
+
+        //reward
+        //= sr * er
+        //= sr * (d + e(1 - d))
+        //= sr * (v(1 - sr) + e(1 - v(1 - sr))
+        //= sr * (v - v*sr + e - ev + sr*v*e)
+        //= sr*v - v*sr^2 + e * sr - ev * sr + sr^2*v*e
+        //= sr^2v(e - 1) + sr(v + e - ev)
+        //= sr^2v(e - 1) + sr(v(1- e) + e)
+
+        //v(sr - sr^2 - e*sr + sr^2*e) + e*sr
+        //=v((e - 1)sr^2 - (e - 1)sr) + e*sr
+        //=v((e - 1)(sr^2 - sr) + e*sr
+        //=v*sr(e - 1)(sr - 1) + e * sr
+        //=sr(v(e - 1)(sr - 1) + e)
+        //=sr(e + v(1 - e)(1 - sr))
+
+        //=sr * er
+        //=sr(d + e(1 - d))
+        //=sr(d + e - ed)
+        //=sr((1-sr) + e - e(1-sr))
+        //=sr(1 - sr + e - e + esr)
+        //=sr(1 - sr + esr)
+        //=sr(1 - sr(1 - e))
+        //=sr - sr^2 + sre
+        //=-sr^2 +sr(1 + e)
+
+        //sr = (1 + e) / 2 일때 최대
+        //sr = 0.5f + 0.5e
+
+        // sr * v + e * (1 - sr * v);
+        // d = sr * target/max;
+        //d + e * (1 - d)
+
+        //reward
+        //= sr(d + e(1 - d))
+        //= sr(srv + e(1 - srv))
+        //= vsr^2 + esr - sr^2ev
+        //= sr^2(v - ev) + esr
+        //= vSr^2(1 - e) + esr
+        //= Sr(2v(1 - e)Sr + e)
+
+        //2V(1 - e)Sr + e
+    }
+
+    private ArticulationBody GetRandomParts()
+    {
+        int randNum = Random.Range(0, bipedalController.controllers.Count);
+
+        return bipedalController.controllers[randNum].articulationBody;
+    }
+
+    private void CalculateAverageSpeedReward(float speedReward)
+    {
+        float rewardSum = averageSpeedReward * CurStep;
+        rewardSum += speedReward;
+        averageSpeedReward = rewardSum / ++CurStep;
+    }
+
+    private void OptimizeMaxSpeed()
+    {
+        if (bipedalController.TargetVelocity < setting.maxSpeed * 0.9)
+            return;
+
+        if (CurStep < 300)
+            return;
+
+        if (averageSpeedReward < 0.5f)
+            return;
+
+
+        setting.maxSpeed *= 1.1f;
     }
 }
 }
---
+++ Assets/Scripts/Agent/BipedalAgentSetting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Agent
+{
+public class BipedalAgentSetting : MonoBehaviour
+{
+    public float minSpeed;
+    public float maxSpeed;
+
+    public float forcePower;
+    public float forceInterval;
+
+    public static BipedalAgentSetting instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+}
+}
