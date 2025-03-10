d0a88ad73d3eae9948f32ea9168f07ce263ef1ac
Park SangHyeok <tkdgurehf00@gmail.com>
2025-03-10 09:14:44 +0900

    Add curTargetSpeed

--- Assets/Scripts/Agent/BipedalAgentUI.cs
+++ Assets/Scripts/Agent/BipedalAgentUI.cs
@@ -1,22 +1,26 @@
 ﻿using Controller;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Agent
 {
 public class BipedalAgentUI : MonoBehaviour
 {
     public TextMeshProUGUI maxSpeed;
+    public TextMeshProUGUI curTargetSpeed;
     public TextMeshProUGUI speedReward;
     public TextMeshProUGUI speedReward_avg;
     public TextMeshProUGUI episodeLength;
 
     public BipedalAgent02 bipedalAgent;
     public BipedalController02 bipedalController;
+    public BipedalAgentSetting setting;
 
     void Update()
     {
-        maxSpeed.text = bipedalAgent.maxSpeed.ToString();
+        maxSpeed.text = setting.maxSpeed.ToString();
+        curTargetSpeed.text = bipedalController.TargetVelocity.ToString();
         speedReward.text = bipedalController.VelocityAccuracy2.ToString();
         speedReward_avg.text = bipedalAgent.Avg_speedReward.ToString();
         episodeLength.text = bipedalAgent.CurStep.ToString();
