c224966744dd38dc371f615a4d2a8d763f69d316
Park SangHyeok <tkdgurehf00@gmail.com>
2025-03-13 16:39:29 +0900

    Add SymmetryQuadPedAgentUI

--- Assets/Scripts/Agent/SymmetryQuadPed/SymmetryQuadPedAgent.cs
+++ Assets/Scripts/Agent/SymmetryQuadPed/SymmetryQuadPedAgent.cs
@@ -8,13 +8,19 @@ namespace Agent.SymmetryQuadPed
 {
 public class SymmetryQuadPedAgent : LegAgent
 {
+    public SymmetryQuadPedUI ui;
+
     protected override float GetReward()
     {
         float speedReward = legController.VelocityAccuracy;
         float efficiency = legController.Efficiency;
         float difficulty = speedReward * (legController.TargetVelocity / setting.maxSpeed);
         float effortReward = speedReward * (difficulty + efficiency * (1 - difficulty));
-        return speedReward * effortReward;
+        float reward = speedReward * effortReward;
+
+        ui?.UpdateParameters(legController.TargetVelocity, speedReward, effortReward, reward);
+
+        return reward;
     }
 }
 }
---
+++ Assets/Scripts/Agent/SymmetryQuadPed/SymmetryQuadPedUI.cs
@@ -0,0 +1,66 @@
+﻿using Controller;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Agent.SymmetryQuadPed
+{
+public class SymmetryQuadPedUI : MonoBehaviour
+{
+    public TextMeshProUGUI maxSpeed;
+    public TextMeshProUGUI curTargetSpeed;
+    public TextMeshProUGUI speedReward;
+    public TextMeshProUGUI speedReward_avg;
+    public TextMeshProUGUI effortReward;
+    public TextMeshProUGUI totalReward;
+    public TextMeshProUGUI episodeLength;
+
+    public Slider curTargetSpeedSlider;
+    public Slider speedRewardSlider;
+    public Slider speedReward_avgSlider;
+    public Slider effortRewardSlider;
+    public Slider totalRewardSlider;
+    public Slider episodeLengthSlider;
+
+    public LegAgent LegAgent;
+    public AgentSetting setting;
+
+    private float agentTargetSpeed;
+    private float agentSpeedReward;
+    private float agentEffortReward;
+    private float agentPelvisReward;
+    private float agentFootReward;
+    private float agentTotalReward;
+
+    void Start()
+    {
+        setting = AgentSetting.instance;
+    }
+
+    void Update()
+    {
+        maxSpeed.text = setting.maxSpeed.ToString();
+        curTargetSpeed.text = agentTargetSpeed.ToString();
+        speedReward.text = agentSpeedReward.ToString();
+        speedReward_avg.text = LegAgent.Avg_speedReward.ToString();
+        effortReward.text = agentEffortReward.ToString();
+        totalReward.text = agentTotalReward.ToString();
+        episodeLength.text = LegAgent.CurStep.ToString();
+
+        curTargetSpeedSlider.value = agentTargetSpeed / setting.maxSpeed;
+        speedRewardSlider.value = agentSpeedReward;
+        speedReward_avgSlider.value = LegAgent.Avg_speedReward;
+        effortRewardSlider.value = agentEffortReward;
+        totalRewardSlider.value = agentEffortReward;
+        episodeLengthSlider.value = LegAgent.CurStep / LegAgent.MaxStep;
+    }
+
+    public void UpdateParameters(float targetSpeed, float speed, float effort, float total)
+    {
+        agentTargetSpeed = targetSpeed;
+        agentSpeedReward = speed;
+        agentEffortReward = effort;
+        agentTotalReward = total;
+    }
+}
+}
