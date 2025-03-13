aa2ff2b2ad48d24c88445aaa026746f1d0d6cfdf
Park SangHyeok <tkdgurehf00@gmail.com>
2025-03-13 13:41:57 +0900

    Commit agent's Base, Controller's Base

---
+++ Assets/Scripts/Agent/LegAgent.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using Randomables;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Agent
+{
+public abstract class LegAgent : Unity.MLAgents.Agent
+{
+    public Arena arena;
+    private LegController legController;
+
+    private Vector3 startPos;
+    private List<float> backUpPos = new List<float>();
+    private List<float> backUpVel = new List<float>();
+    private List<ArticulationDrive> startState = new();
+
+    public AgentSetting setting;
+
+    private float averageSpeedReward = 0;
+    public float Avg_speedReward => averageSpeedReward;
+    public float CurStep { private set; get; } = 0;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        legController = GetComponent<LegController>();
+    }
+
+    private void Start()
+    {
+        setting = AgentSetting.instance;
+
+        startPos = transform.position;
+        legController.root.GetJointPositions(backUpPos);
+        legController.root.GetJointVelocities(backUpVel);
+
+        legController.controllers.ForEach(controller => startState.Add(controller.articulationBody.xDrive));
+
+       arena.Rand();
+    }
+
+    private void ResetBody()
+    {
+        legController.root.TeleportRoot(startPos, Quaternion.Euler(0, Random.Range(0.0f, 360.0f), 0));
+        legController.root.SetJointPositions(backUpPos);
+        legController.root.SetJointVelocities(backUpVel);
+
+        legController.root.linearVelocity = Vector3.zero;
+        legController.root.angularVelocity = Vector3.zero;
+        foreach (var controller in legController.controllers)
+        {
+            controller.articulationBody.linearVelocity = Vector3.zero;
+            controller.articulationBody.angularVelocity = Vector3.zero;
+        }
+
+        int i = 0;
+        legController.controllers.ForEach(controller => controller.articulationBody.xDrive = startState[i++]);
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        OptimizeMaxSpeed();
+        ResetBody();
+
+        legController.TargetVelocity = Mathf.Pow(Random.Range(0, 1f), 0.5f) * (setting.maxSpeed - setting.minSpeed) + setting.minSpeed;
+        arena.TargetObject.Rand();
+
+        averageSpeedReward = 0f;
+        CurStep = 0f;
+    }
+
+    private void OptimizeMaxSpeed()
+    {
+        if (legController.TargetVelocity < setting.maxSpeed * 0.9)
+            return;
+
+        if (CurStep < MaxStep * 0.9f)
+            return;
+
+        if (averageSpeedReward < 0.5f)
+            return;
+
+
+        setting.maxSpeed *= 1.1f;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+        => legController.CollectObservations(sensor);
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        var actions = actionBuffers.ContinuousActions;
+
+        int abCount = legController.controllers.Count;
+        float[] forceRatios = new float[abCount];
+        float[] targets = new float[abCount];
+
+        int i = 0;
+        for (int index = 0; index < abCount; index++)
+        {
+            forceRatios[index] = (actions[i++] + 1) * 0.5f;
+            targets[index] = (actions[i++] + 1) * 0.5f;
+        }
+
+        legController.SetDrive(forceRatios, targets);
+    }
+
+    private bool IsTargetTouched()
+    {
+        var positionDelta = (transform.position - arena.TargetObject.transform.position);
+        positionDelta.y = 0f;
+        float distance = positionDelta.magnitude;
+        return distance < 0.5f;
+    }
+
+    protected abstract float GetReward();
+
+    private void CalculateAverageSpeedReward(float speedReward)
+    {
+        float rewardSum = averageSpeedReward * CurStep;
+        rewardSum += speedReward;
+        averageSpeedReward = rewardSum / ++CurStep;
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsTargetTouched())
+        {
+            arena.TargetObject.Rand();
+            AddReward(1f);
+        }
+
+        CalculateAverageSpeedReward(legController.VelocityAccuracy);
+        AddReward(GetReward());
+    }
+}
+}
---
+++ Assets/Scripts/Agent/LegController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Controller;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Agent
+{
+public abstract class LegController : MonoBehaviour
+{
+    public ArticulationBody root;
+
+    public List<DriveController> controllers;
+
+    [SerializeField] protected Vector3 moveDir;
+    [SerializeField] protected float targetVelocity;
+
+    public PowerHub powerHub;
+
+    [SerializeField] protected PositionSupporter supporter;
+
+    public float Efficiency { get; protected set; }
+
+    public Vector3 MoveDir
+    {
+        get => moveDir;
+        set => moveDir = value.normalized;
+    }
+
+    public float TargetVelocity
+    {
+        get => targetVelocity;
+        set
+        {
+            if (value < 0)
+                throw new Exception("The speed is Out of Range");
+
+            targetVelocity = value;
+        }
+    }
+
+    public float VelocityAccuracy
+    {
+        get
+        {
+            var myAvgVel = GetAvgVel();
+            var targetVel = MoveDir * targetVelocity;
+
+            var dirAccuracy = Vector3.Dot(myAvgVel.normalized, targetVel.normalized);
+            dirAccuracy = (dirAccuracy + 1f) * 0.5f;
+
+            float mySpeed = myAvgVel.magnitude;
+            float targetSpeed = targetVelocity;
+
+            var speedDelta = Mathf.Clamp(Mathf.Abs(mySpeed - targetSpeed), 0, targetSpeed);
+            var speedAccuracy = Mathf.Clamp01(1 - speedDelta / targetSpeed);
+
+            return dirAccuracy * speedAccuracy;
+        }
+    }
+
+    protected abstract Vector3 GetAvgVel();
+    public abstract void CollectObservations(VectorSensor sensor);
+
+    public void SetDrive(float[] forceRatios, float[] targets)
+    {
+        if (forceRatios.Length != controllers.Count)
+            throw new Exception("forceRatios.length is Not Match Controllers.Count");
+
+        if (targets.Length != controllers.Count)
+            throw new Exception("targets.length is Not Match Controllers.Count");
+
+        float usableForce = powerHub.UsableForce;
+        float requestedForcesSum = 0f;
+
+        int i = 0;
+        controllers.ForEach(controller => requestedForcesSum += controller.MaxForce * forceRatios[i++]);
+
+        i = 0;
+        controllers.ForEach(controller => controller.Target = targets[i++]);
+
+        float forceToUse = requestedForcesSum < usableForce ? requestedForcesSum : usableForce;
+        float usableRatio = requestedForcesSum == 0 ? 1 : forceToUse / requestedForcesSum;
+
+        i = 0;
+        controllers.ForEach(controller => controller.ForceUseRatio = forceRatios[i++] * usableRatio);
+
+        powerHub.ReleaseForce(forceToUse);
+        Efficiency = 1f - forceToUse / usableForce;
+    }
+}
+}
