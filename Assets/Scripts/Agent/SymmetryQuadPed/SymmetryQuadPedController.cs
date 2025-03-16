ed22777b8b71278f76056d9e2d3451f04284ca5e
Park SangHyeok <tkdgurehf00@gmail.com>
2025-03-16 21:45:03 +0900

    Delete FootJoint, Add Additional parameters

--- Assets/Scripts/Agent/SymmetryQuadPed/SymmetryQuadPedController.cs
+++ Assets/Scripts/Agent/SymmetryQuadPed/SymmetryQuadPedController.cs
@@ -11,28 +11,29 @@ public class SymmetryQuadPedController : LegController
     public DriveController thighY1;
     public DriveController thighX1;
     public DriveController shine1;
-    public DriveController foot1;
 
     public DriveController thighY2;
     public DriveController thighX2;
     public DriveController shine2;
-    public DriveController foot2;
 
     public DriveController thighY3;
     public DriveController thighX3;
     public DriveController shine3;
-    public DriveController foot3;
 
     public DriveController thighY4;
     public DriveController thighX4;
     public DriveController shine4;
+
+    public DriveController foot1;
+    public DriveController foot2;
+    public DriveController foot3;
     public DriveController foot4;
 
     public override float LookDot
     {
         get
         {
-            var r1 = Quaternion.LookRotation(root.transform.position + moveDir).eulerAngles;
+            var r1 = Quaternion.LookRotation(moveDir).eulerAngles;
             var r2 = root.transform.rotation.eulerAngles;
 
             var delta = Mathf.Abs(r1.y - r2.y);
@@ -49,22 +50,18 @@ public class SymmetryQuadPedController : LegController
         controllers.Add(thighY1);
         controllers.Add(thighX1);
         controllers.Add(shine1);
-        controllers.Add(foot1);
 
         controllers.Add(thighY2);
         controllers.Add(thighX2);
         controllers.Add(shine2);
-        controllers.Add(foot2);
 
         controllers.Add(thighY3);
         controllers.Add(thighX3);
         controllers.Add(shine3);
-        controllers.Add(foot3);
 
         controllers.Add(thighY4);
         controllers.Add(thighX4);
         controllers.Add(shine4);
-        controllers.Add(foot4);
     }
 
     protected override Vector3 GetAvgVel()
@@ -101,13 +98,35 @@ public class SymmetryQuadPedController : LegController
             sensor.AddObservation(controller.Acceleration);
         }
 
+        RaycastHit hit;
+        float maxRaycastDist = 1;
+        if (Physics.Raycast(root.transform.position, Vector3.down, out hit, maxRaycastDist))
+        {
+            sensor.AddObservation(hit.distance / maxRaycastDist);
+        }
+        else
+            sensor.AddObservation(1);
+
+        sensor.AddObservation(supporter.transform.InverseTransformDirection(foot1.transform.position - supporter.transform.position));
+        sensor.AddObservation(supporter.transform.InverseTransformDirection(foot2.transform.position - supporter.transform.position));
+        sensor.AddObservation(supporter.transform.InverseTransformDirection(foot3.transform.position - supporter.transform.position));
+        sensor.AddObservation(supporter.transform.InverseTransformDirection(foot4.transform.position - supporter.transform.position));
+
         sensor.AddObservation(Vector3.Distance(GetAvgVel(), targetVelocity * MoveDir));
         sensor.AddObservation(supporter.transform.InverseTransformDirection(GetAvgVel()));
         sensor.AddObservation(supporter.transform.InverseTransformDirection(targetVelocity * MoveDir));
+        sensor.AddObservation(Vector3.Dot(root.transform.up, Vector3.up));
 
-        var relativePos = root.transform.InverseTransformPoint(targetObject.transform.position);
-        sensor.AddObservation(relativePos);
-        sensor.AddObservation(Quaternion.LookRotation(relativePos));
+        var r1 = Quaternion.LookRotation(moveDir).eulerAngles;
+        var r2 = root.transform.rotation.eulerAngles;
+
+        var delta = Mathf.Abs(r1.y - r2.y);
+        delta = delta < 180 ? delta : delta - 360;
+        delta /= 180;
+        sensor.AddObservation(delta);
+
+        sensor.AddObservation(supporter.transform.InverseTransformDirection(root.linearVelocity));
+        sensor.AddObservation(supporter.transform.InverseTransformDirection(root.angularVelocity));
     }
 }
 }
