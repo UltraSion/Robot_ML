117054bf85b62c8dc998b5c87704aa98e0f87187
Park SangHyeok <tkdgurehf00@gmail.com>
2025-03-16 21:44:25 +0900

    Add symmetry Reward

--- Assets/Scripts/Agent/SymmetryQuadPed/SymmetryQuadPedAgent.cs
+++ Assets/Scripts/Agent/SymmetryQuadPed/SymmetryQuadPedAgent.cs
@@ -10,6 +10,35 @@ namespace Agent.SymmetryQuadPed
 public class SymmetryQuadPedAgent : LegAgent
 {
     public SymmetryQuadPedUI ui;
+    public SymmetryQuadPedController LegController => legController as SymmetryQuadPedController;
+
+    public float SymmetryReward
+    {
+        get
+        {
+            var controller = LegController;
+
+            var leg1Forward = controller.thighY1.transform.forward;
+            var leg2Forward = controller.thighY2.transform.forward;
+            var leg3Forward = controller.thighY3.transform.forward;
+            var leg4Forward = controller.thighY4.transform.forward;
+
+            leg1Forward.y = 0;
+            leg2Forward.y = 0;
+            leg3Forward.y = 0;
+            leg4Forward.y = 0;
+
+            leg1Forward.Normalize();
+            leg2Forward.Normalize();
+            leg3Forward.Normalize();
+            leg4Forward.Normalize();
+
+            float r1 = (Vector3.Dot(leg1Forward, -leg3Forward) + 1f) * 0.5f;
+            float r2 = (Vector3.Dot(leg2Forward, -leg4Forward) + 1f) * 0.5f;
+
+            return r1 * r2;
+        }
+    }
 
     protected override void RandTargetVelocity()
     {
@@ -23,7 +52,8 @@ public class SymmetryQuadPedAgent : LegAgent
         float difficulty = speedReward * (legController.TargetVelocity / setting.maxSpeed);
         float effortReward = speedReward * (difficulty + efficiency * (1 - difficulty));
         float lookReward = legController.LookDot;
-        float reward = speedReward * effortReward * lookReward;
+        float symmetryReward = SymmetryReward;
+        float reward = speedReward * effortReward * lookReward * symmetryReward;
 
         ui?.UpdateParameters(legController.TargetVelocity, speedReward, effortReward, reward);
 
